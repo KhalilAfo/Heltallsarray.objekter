77b1169e3349b9bf108a17540a8ac64461a473a2
KhalilAfo <78377850+KhalilAfo@users.noreply.github.com>
2022-05-22 22:57:47 +0200

    Initial commit

---
+++ src/main/resources/static/array.js
@@ -0,0 +1,19 @@
+const liste = [1,3,5,2,7,-9];
+
+liste[0]=10;
+
+for(let i=0;i<liste.length;i++){
+    console.log(liste[i]);
+}
+
+for(let i of liste) {
+    console.log(i);
+}
+
+liste.push(23); // setter inn heltallet 23 etter siste verdi i arrayet
+lengde=liste.length; // gir lengden på arrayet
+liste.splice(1,2); // sletter to verdier i arrayet fra og med plass 1
+const posisjon=liste.indexOf(23); // gir posisjonen i arrayet hvor verdien er 23 (nummer 4)
+liste.sort(); // sorterer arrayet
+console.log(liste);
+console.log(posisjon);
